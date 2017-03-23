85f88b8330cc52024de02b7b756a4cd5a13c28ee
Renan Boni <Renan Boni>
2017-03-23 13:12:38 -0300

    comparacao

---
+++ merge_multi.c
@@ -0,0 +1,125 @@
+//
+//
+
+#include <stdio.h>
+#include <omp.h>
+void merging(int low, int mid, int high,int *vetorR, int *lastResult) {
+    int l1, l2, i;
+    
+    for(l1 = low, l2 = mid + 1, i = low; l1 <= mid && l2 <= high; i++) {
+        if(vetorR[l1] <= vetorR[l2])
+            lastResult[i] = vetorR[l1++];
+        else
+            lastResult[i] = vetorR[l2++];
+    }
+    
+    while(l1 <= mid)
+        lastResult[i++] = vetorR[l1++];
+    
+    while(l2 <= high)
+        lastResult[i++] = vetorR[l2++];
+    
+    for(i = low; i <= high; i++)
+        vetorR[i] = lastResult[i];
+    
+    return;
+}
+
+
+void sort(int low, int high,int *vetorR, int *lastResult) {
+    int mid;
+    if(low < high) {
+        mid = (low + high) / 2;
+        #pragma omp parallel 
+        {
+            #pragma omp sections nowait
+            {
+                #pragma omp section
+                    sort(low, mid, vetorR, lastResult);
+                #pragma omp section
+                    sort(mid+1, high, vetorR, lastResult);
+            }
+        }
+        merging(low, mid, high, vetorR, lastResult);
+    } else {
+        return;
+    }
+}
+
+
+int * concatena(int tam1, int *vetor1, int tam2, int *vetor2) {
+    
+    int  result[tam1+tam2];
+    int  lastResult[tam1+tam2];
+    int i=0, j=0, k=0;
+    
+    while(i < tam1) {
+        result[k] = vetor1[i];
+        i = i + 1;
+        k = k + 1;
+    }
+    
+    while(j < tam2) {
+        result[k] = vetor2[j];
+        j = j + 1;
+        k = k + 1;
+    }
+    
+    k = 0;
+    
+    sort(k,(tam1+tam2 - 1), result, lastResult);
+    
+    return lastResult;
+}
+
+/* main function to call above defined function */
+int main (int argc, char **argv) {
+    
+    //Array1.txt
+    //Array2.txt
+    //Resultado.txt
+    
+    FILE *file = fopen(argv[1], "r");
+    int array1[5000];
+    
+    FILE *fileR = fopen(argv[3], "w");
+    int *r;
+    int h;
+    
+    int i=0;
+    int num;
+    while(fscanf(file, "%d", &num) > 0) {
+        array1[i] = num;
+        i++;
+    }
+    fclose(file);
+    
+    file = fopen(argv[2], "r");
+    int array2[5000];
+    
+    int j=0;
+    int num2;
+    while(fscanf(file, "%d", &num2) > 0) {
+        array2[j] = num2;
+        j++;
+    }
+    fclose(file);
+    
+    r = concatena(i,array1,j,array2);
+    
+    int anterior = 0;
+    
+    for ( h = 0; h < (i+j); h++ ) {
+        if(r[h] == anterior) {
+            
+        } else {
+            fprintf(fileR, "%d\n", r[h]);
+        }
+        anterior = r[h];
+    }
+    fclose(file);
+    
+    return 0;
+}
+
+
---
+++ quick_sort.c
@@ -0,0 +1,158 @@
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <omp.h>
+
+int * concatena(int tam1, int *vetor1, int tam2, int *vetor2);
+void * elimina_repetidos(int size, int * result, int * newSize);
+int * fillVector(char * arg, int * size);
+void insertion_sort(int size, int* list);
+void quick_sort(int l, int r, int* list);
+void swap(int a, int b, int* list);
+
+double PCFreq = 0.0;
+double start, end;
+double cpu_time_used;
+
+int main(int argc, char* argv[])
+{
+	int size1, size2;
+	
+	int * vector1 = fillVector(argv[1], &size1);
+	int * vector2 = fillVector(argv[2], &size2);
+	
+	int * result = concatena(size1, vector1, size2, vector2);
+	
+	start = omp_get_wtime();
+	quick_sort(0, (size1 + size2) - 1, result);
+	end = omp_get_wtime();
+    printf("Tempo = %.7lf milissegundos\n", end-start);
+	
+	int newSize;
+	elimina_repetidos(size1 + size2, result, &newSize);	
+	
+	FILE * file = fopen(argv[3], "a+");
+	
+	if(!file)
+		exit(EXIT_FAILURE);
+	
+	int i;
+	for(i = 0; i < newSize; i++)
+		fprintf(file, "%d ", result[i]);	
+	
+	return 0;
+}
+int * concatena(int tam1, int *vetor1, int tam2, int *vetor2)
+{
+	int size = tam1 + tam2;
+	
+	int * result = (int*)calloc(size, sizeof(int));
+	
+	int i, j;
+	for(i = 0; i < tam1; i++)
+		result[i] = vetor1[i];
+		
+	for(j = 0; j < tam2; j++)
+		result[i + j] = vetor2[j];
+	
+	return result;
+}
+void * elimina_repetidos(int size, int * result, int * newSize)
+{
+	int n = size;
+
+	int i, j, k;
+	for(i = 0; i < n; i++)
+	{
+		for(j = i + 1; j < n; j++)
+		{
+			if(result[i] == result[j])
+			{
+				for(k = j + 1; k < n; k++)
+					result[k-1] = result[k];
+
+				n--;
+				j--;
+			}
+		}
+	}	
+	*newSize = n;
+}
+int * fillVector(char * arg, int * size)
+{
+	//Open file
+	FILE * file = fopen(arg, "r");
+	
+	if(!file)
+		exit(EXIT_FAILURE);
+	
+	//Count number of integers
+	int n, i;
+	i = 0;
+	
+	while(fscanf(file, "%d", &n) != EOF)
+		i++;
+	
+	int * numbers = (int*)calloc(i, sizeof(int));
+	*size = i;
+	i = 0;
+	
+	fseek(file, 0, SEEK_SET);
+	
+	//Read integers
+	while(fscanf(file, "%d", &n) != EOF)
+	{
+		numbers[i] = n;
+		i++;
+	}
+	
+	fclose(file);
+	
+	return numbers;
+}
+
+void quick_sort(int l, int r, int* list)
+{
+    if (l < r) // $a0 < $a1
+    {
+        int p = (l + r) / 2; // $s0 = ($a0 + $a1) / 2
+        int pos = partition(l, r, p, list);
+        
+        quick_sort(l, pos-1, list);
+        quick_sort(pos+1, r, list);
+    }
+}
+
+int partition(int l, int r, int p, int* list)
+{
+    int pos, i; // $s0, $s1
+    
+    swap(p, r, list);
+    
+    i = l; // $s1 = $a0
+    pos = l; // $s0 = $a0
+    
+    while (i < r) // $s1 < $a1
+    {
+        if (list[i] < list[r])
+        {
+            swap(i, pos, list);
+            pos++; // $s0 = $s0 + 1
+        }
+        
+        i++; // $s1 = $s1 + 1
+    }
+    
+    swap(r, pos, list);
+    
+    return pos;
+}
+
+void swap(int a, int b, int* list)
+{
+    int tmp = list[a]; // $t0 = $a2 ( + $a0)
+    list[a] = list[b]; // $a2 ( + $a0) = $a2 ( + $a1)
+    list[b] = tmp; // $a2 ( + $a1) = $t0
+}
